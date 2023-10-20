09294a94684bd33610005d3d68985915c822b463
Iamneo <techadmin@iamneo.ai>
2023-10-20 18:06:02 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/controller/MedicineController.java
+++ springapp/src/main/java/com/example/springapp/controller/MedicineController.java
@@ -1,32 +1,66 @@
-package com.example.springapp.controller;
+package com.example.springapp.model;
 
-import com.example.springapp.Service.MedicineService;
-import com.example.springapp.model.Medicine;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
-@RestController
-@RequestMapping
-public class MedicineController {
-    @Autowired
-    MedicineService medicineService;
+@Entity
+public class Medicine {
+    @Id
+    int medicineId;
+    String medicineName;
+    float price;
+    int quantity;
+    String description;
 
-    @PostMapping("/medicines")
+    public Medicine() {
+    };
 
-    public ResponseEntity<Boolean> addMedicine(@RequestBody Medicine medicine) {
-        boolean result = medicineService.addMedicine(medicine);
-        return ResponseEntity.ok(result);
+    public Medicine(int medicineId, String medicineName, float price, int quantity, String description) {
+        this.medicineId = medicineId;
+        this.medicineName = medicineName;
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
     }
 
-    @PutMapping("/{medicineId}")
-    public ResponseEntity<Medicine> updateMedicine(@PathVariable int medicineId,
-            @RequestBody Medicine updatedMedicine) {
-        Medicine updated = medicineService.updateMedicine(medicineId, updatedMedicine);
-        if (updated != null) {
-            return ResponseEntity.ok(updated);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public int getMedicineId() {
+        return medicineId;
     }
+
+    public void setMedicineId(int medicineId) {
+        this.medicineId = medicineId;
+    }
+
+    public String getMedicineName() {
+        return medicineName;
+    }
+
+    public void setMedicineName(String medicineName) {
+        this.medicineName = medicineName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
